11835db7de6bdac5619352fc0c51b24c2500c023
TGOlson <olso4052@umn.edu>
2013-08-28 19:51:24 -0500

    fixed bug that allow a call to old_roman with 0

--- lib/methods/new_roman.rb
+++ lib/methods/new_roman.rb
@@ -1,19 +1,17 @@
-require './lib/methods/old_roman'  
-
 def new_roman(year)
   raise ArgumentError.new("Input be greater than 0.") if year < 1
   digits = year.to_s.split('').reverse
   new_roman = []
-  nines_fours = [['IX','IV'], #ones
-   			         ['XC','XL'], #tens
-				         ['CM','CD']] #hundreds
+  nines_fours = [['IX','IV'], # ones
+   			         ['XC','XL'], # tens
+				         ['CM','CD']] # hundreds
 
   digits.each_with_index do |x,i|
     if x == "9"
       new_roman << nines_fours[i][0]
     elsif x == "4"
       new_roman << nines_fours[i][1]
-    else
+    elsif x != 0
       new_roman << old_roman(x.to_i * (10 ** i))
     end
   end
