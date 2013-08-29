19438c8a288045a73427a980db98fb0c410c2f85
TGOlson <olso4052@umn.edu>
2013-08-28 22:41:44 -0500

    before seperation of methods

--- lib/methods/english_number.rb
+++ lib/methods/english_number.rb
@@ -3,48 +3,41 @@ def english_number(number)
   return 'zero' if number == 0
   string = ''
 
-  ones_hash = { 1 => 'one',   2 => 'two',   3 => 'three', 
-  					    4 => 'four',  5 => 'five',  6 => 'six', 
-  					    7 => 'seven', 8 => 'eight', 9 => 'nine'}
+  small_hash   = {  1 => 'one',        2 => 'two',       3 => 'three', 
+  					        4 => 'four',       5 => 'five',      6 => 'six', 
+  					        7 => 'seven',      8 => 'eight',     9 => 'nine',
+                   11 => 'eleven',    12 => 'twelve',   13 => 'thirteen', 
+  					       14 => 'fourteen',  15 => 'fifteen',  16 => 'sixteen', 
+  					       17 => 'seventeen', 18 => 'eighteen', 19 => 'nineteen',
+                   10 => 'ten',       20 => 'twenty',   30 => 'thirty', 
+  					       40 => 'fourty',    50 => 'fifty',    60 => 'sixty', 
+  					       70 => 'seventy',   80 => 'eighty',   90 => 'ninety'}
 
-  teens_hash = { 11 => 'eleven',    12 => 'twelve',   13 => 'thirteen', 
-  					     14 => 'fourteen',  15 => 'fifteen',  16 => 'sixteen', 
-  					     17 => 'seventeen', 18 => 'eighteen', 19 => 'nineteen'}
+  hundo_hash   = { 100 => 'hundred', 1000 => 'thousand', 1000000 => 'million',
+  					       1000000000 => 'billion', 1000000000000 => 'trillion'}
 
-  tens_hash = { 10 => 'ten',     20 => 'twenty', 30 => 'thirty', 
-  					    40 => 'fourty',  50 => 'fifty',  60 => 'sixty', 
-  					    70 => 'seventy', 80 => 'eighty', 90 => 'ninety'}
-
-  hundreds_hash = { 100 => 'hundred', 1000 => 'thousand', 1000000 => 'million',
-  					        1000000000 => 'billion', 1000000000000 => 'trillion'}
 extra = number
 
 while extra != 0
 
-  length = number.to_s.length
-  factor = 10 ** (length - 1)
-  lead = number / factor
-  mag  = (number - (number % factor)) / lead
-  extra = number - (lead * mag)
- # puts [number, lead, mag, extra].inspect - test for loops
+  factor = 10 ** (number.to_s.length - 1)
+  lead_digit = number / factor
+  mag  = (number - (number % factor)) / lead_digit
+  extra = number - (lead_digit * mag)
+  puts [number, lead_digit, mag, extra].inspect # test for loops
 
   if number > 100  
-    string += ones_hash[lead] + ' ' + hundreds_hash[mag]
+    string += small_hash[lead_digit] + ' ' + hundo_hash[mag]
   end
 
-  if number < 100 && number > 19
-    string += tens_hash[number - extra]
+  if number < 100 && number >= 20
+    string += small_hash[number - extra]
   end
 
-    if number < 20 && number > 10
-      string += teens_hash[number]
-      return string
-    end
-
-    if number < 10
-      string += ones_hash[number]
-      return string
-    end
+  if number < 20
+    string += small_hash[number]
+    return string
+  end
 
   number = extra
   string += ' '
@@ -53,9 +46,9 @@ end
   string
 end
 
-#puts english_number(0)
-#puts english_number(50)
-#puts english_number(517)
-#puts english_number(1003)
-#puts english_number(1999)
-#puts english_number(5720)
+puts english_number(0)
+puts english_number(50)
+puts english_number(517)
+puts english_number(1003)
+puts english_number(1999)
+puts english_number(50720)
