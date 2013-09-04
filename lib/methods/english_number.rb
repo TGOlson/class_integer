f625f0f6b8ff2618ec6d7285585f15e2003ee548
TGOlson <olso4052@umn.edu>
2013-09-04 16:15:02 -0500

    Seperated methods, works up to billions

--- lib/methods/english_number.rb
+++ lib/methods/english_number.rb
@@ -2,46 +2,36 @@ def english_number(number)
   raise ArgumentError.new("Input be greater than or equal to 0.") if number < 0
   return 'zero' if number == 0
   string = ''
+  puts number
 
-  extra = number
+  factor = number.to_s.length - 1
 
-  while extra != 0
-
-    factor = number.to_s.length - 1
-    lead_digit = number / (10 ** factor)
-    extra = number - (lead_digit * (10 ** factor))
-    puts [number, lead_digit, factor, extra].inspect # test for loops
-
-    if factor > 3
-      puts lead_digit = number / (10 ** ((factor / 3) * 3))
-      string += small_number(lead_digit - lead_digit % 10)
-      string += ' ' + small_number(lead_digit % 10) if lead_digit % 10 != 0
-      string += ' ' + hundreds(factor - (factor / 3))
-      extra = number - (lead_digit * 10 ** ((factor / 3) * 3))
+  if factor >= 2
+    if factor >= 9 # add billions
+      lead_digits = (number % 10 ** 12) / 10 ** 9
+      string += parser(lead_digits) + ' ' + factors(9) + ' '
+    end
+    if factor >= 6 # add millions
+      lead_digits = (number % 10 ** 9) / 10 ** 6
+      string += parser(lead_digits) + ' ' + factors(6) + ' '
+    end
+    if factor >= 3 # add thousands
+      lead_digits = (number % 10 ** 6) / 10 ** 3
+      string += parser(lead_digits) + ' ' + factors(3) + ' '
     end
 
-    if factor > 1 && factor <= 3
-      string += small_number(lead_digit) + ' ' + hundreds(factor)
-    end
-
-    if number < 100 && number >= 20
-      string += small_number(number - extra)
-    end
-
-    if number < 20
-      string += small_number(number)
-      return string
-    end
-
-    number = extra
-    string += ' '
+    string += 'and ' if number % 1000 < 100
+    string += parser(number % 1000) # last three digits
+  else
+    string += small_number(number)
   end
   
   string
 end
 
 
-def small_number(number)
+def small_number(number) #number under 99
+  string = ''
   small_hash   = {  1 => 'one',        2 => 'two',       3 => 'three', 
                     4 => 'four',       5 => 'five',      6 => 'six', 
                     7 => 'seven',      8 => 'eight',     9 => 'nine',
@@ -51,18 +41,30 @@ def small_number(number)
                    10 => 'ten',       20 => 'twenty',   30 => 'thirty', 
                    40 => 'fourty',    50 => 'fifty',    60 => 'sixty', 
                    70 => 'seventy',   80 => 'eighty',   90 => 'ninety'}
-  small_hash[number]
+
+  extra = number % 10
+  string += small_hash[number - extra] + ' ' if number != extra
+  string += small_hash[extra] if extra != 0
+  string
 end
 
+def parser(number)
+  string = ''
+  string += hundreds(number) if number >= 100
+  string += small_number(number) if number < 100
+  string
+end 
+
 def hundreds(number)
+  string = ''
+  string += small_number(number / 100 )
+  string += ' ' + factors(2)
+  string += ' and ' + small_number(number % 100) if number % 100 != 0
+  string
+end
+
+def factors(number) # number greater than or equal to 100
   hundred_hash = { 2 => 'hundred', 3 => 'thousand', 6 => 'million',
                    9 => 'billion', 12 => 'trillion'}
   hundred_hash[number]
 end
-
-puts english_number(0)
-puts english_number(50)
-puts english_number(517)
-puts english_number(1003)
-puts english_number(1999)
-puts english_number(55720)
