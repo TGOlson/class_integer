384f1a4559441ab236071446d5511d4cbec1ade1
TGOlson <tydotg@gmail.com>
2013-10-12 17:16:12 -0700

    Updated #to_english, and included number_library file

--- lib/methods/english_number.rb
+++ lib/methods/english_number.rb
@@ -1,70 +1,19 @@
-def english_number(number)
-  raise ArgumentError.new("Input be greater than or equal to 0.") if number < 0
-  return 'zero' if number == 0
-  string = ''
+# Solution for Challenge: P1: Methods and Recursion - Numbers in Words. Started 2013-10-12T22:47:29+00:00
+require_relative 'number_library'
 
-  factor = number.to_s.length - 1
-
-  if factor >= 2
-
-    factor_check = [12,9,6,3] # trillions, billions, millions, thousands
-    factor_check.each do |x|
-      if factor >= x
-        lead_digits = (number % 10 ** (x + 3)) / 10 ** x # remove numbers from higher and lower factors
-        string += parser(lead_digits) + ' ' + factors(x) + ' ' if lead_digits != 0
-      end
+def english_number(num)
+  raise ArgumentError.new("Input be greater than or equal to 0.") if num < 0
+  return 'one' if num == 1
+  to_words_array = []
+ 
+  NUM_WORD_PAIRS.each do |pair|
+    if num > 99
+      lead_digits = (num % (pair[0] * 1000)) / pair[0]
+      to_words_array << english_number(lead_digits) if lead_digits > 0
     end
-
-    string += 'and ' if ((number % 1000 < 100) && (number % 1000 != 0))
-    string += parser(number % 1000) # last three digits
-    string.chop! if number % 1000 == 0 # remove extra space if no ones or tens
-  else
-    string += small_number(number)
+    to_words_array << pair[1] if (num / pair[0]) > 0
+    num %= pair[0]
   end
-  
-  string
-end
-
-
-def small_number(number) # numbers under 99
-  string = ''
-  small_hash   = {  1 => 'one',        2 => 'two',       3 => 'three', 
-                    4 => 'four',       5 => 'five',      6 => 'six', 
-                    7 => 'seven',      8 => 'eight',     9 => 'nine',
-                   11 => 'eleven',    12 => 'twelve',   13 => 'thirteen', 
-                   14 => 'fourteen',  15 => 'fifteen',  16 => 'sixteen', 
-                   17 => 'seventeen', 18 => 'eighteen', 19 => 'nineteen',
-                   10 => 'ten',       20 => 'twenty',   30 => 'thirty', 
-                   40 => 'fourty',    50 => 'fifty',    60 => 'sixty', 
-                   70 => 'seventy',   80 => 'eighty',   90 => 'ninety'}
-
-  if number < 20
-    return small_hash[number]
-  else
-    extra = number % 10
-    string += small_hash[number - extra]
-    string += ' ' + small_hash[extra] if extra != 0
-  end
-  string
-end
-
-def parser(number) # decides is lead_digits needs to be processed as hundreds, or a small number
-  string = ''
-  string += hundreds(number) if number >= 100
-  string += small_number(number) if number < 100 && number != 0
-  string
-end 
-
-def hundreds(number)
-  string = ''
-  string += small_number(number / 100 )
-  string += ' ' + factors(2)
-  string += ' and ' + small_number(number % 100) if number % 100 != 0
-  string
-end
-
-def factors(number) # 10 ** factor
-  hundred_hash = { 2 => 'hundred', 3 => 'thousand', 6 => 'million',
-                   9 => 'billion', 12 => 'trillion'}
-  hundred_hash[number]
+ 
+  to_words_array.join(' ')
 end
--- test.rb
+++ test.rb
@@ -3,6 +3,6 @@ require './lib/class_Integer'
 output = [1999.to_roman     == 'MCMXCIX',
           1999.to_old_roman == 'MDCCCCLXXXXVIIII',
           4.fact            == 24,
-          1999.to_english   == 'one thousand nine hundred and ninety nine',
+          1999.to_english   == 'one thousand nine hundred ninety nine',
           1999.add_commas   == '1,999']
 p output
